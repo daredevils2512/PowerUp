d746c8a2aca34e427012bfd7248b764ef1b041f4
Chris Morton <cmorton@saturnsys.com>
2018-02-10 10:46:57 -0600

    get climber back to master

--- src/Commands/ClimberRunWing.cpp
+++ src/Commands/ClimberRunWing.cpp
@@ -1,34 +1,35 @@
-#include "ClimberLeftWingRun.h"
+#include "Commands/ClimberRunWing.h"
 
-ClimberLeftWingRun::ClimberLeftWingRun(double speed) {
+ClimberRunWing::ClimberRunWing(Climber::ClimberWing wing, double speed) {
 	// Use Requires() here to declare subsystem dependencies
 	// eg. Requires(Robot::chassis.get());
 	Requires (Robot::climber.get());
 	m_speed = speed;
+	m_wing = wing;
 }
 
 // Called just before this Command runs the first time
-void ClimberLeftWingRun::Initialize() {
+void ClimberRunWing::Initialize() {
 
 }
 
 // Called repeatedly when this Command is scheduled to run
-void ClimberLeftWingRun::Execute() {
-	Robot::climber->SetLeftWingSpeed(m_speed);
+void ClimberRunWing::Execute() {
+	Robot::climber->SetWingSpeed(m_wing, m_speed);
 }
 
 // Make this return true when this Command no longer needs to run execute()
-bool ClimberLeftWingRun::IsFinished() {
+bool ClimberRunWing::IsFinished() {
 	return true;
 }
 
 // Called once after isFinished returns true
-void ClimberLeftWingRun::End() {
-	Robot::climber->SetLeftWingSpeed(0.0);
+void ClimberRunWing::End() {
+	Robot::climber->SetWingSpeed(m_wing, 0.0);
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
-void ClimberLeftWingRun::Interrupted() {
+void ClimberRunWing::Interrupted() {
 
 }
