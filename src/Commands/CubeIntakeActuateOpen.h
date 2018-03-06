d99a1720b9c6c56a0b36192401da58878b910a08
Chris Morton <cmorton@saturnsys.com>
2018-03-06 10:57:21 -0600

    updated

---
+++ src/Commands/CubeIntakeActuateOpen.h
@@ -0,0 +1,21 @@
+/*
+ * CubeIntakeActuateOpen.h
+ *
+ *  Created on: Mar 5, 2018
+ *      Author: cmorton
+ */
+
+#ifndef SRC_COMMANDS_CUBEINTAKEACTUATEOPEN_H_
+#define SRC_COMMANDS_CUBEINTAKEACTUATEOPEN_H_
+
+#include <Commands/CubeIntakeActuate.h>
+
+class CubeIntakeActuateOpen: public CubeIntakeActuate {
+public:
+	CubeIntakeActuateOpen();
+	virtual ~CubeIntakeActuateOpen();
+protected:
+	virtual frc::DoubleSolenoid::Value getDirection(){return frc::DoubleSolenoid::kForward;}
+};
+
+#endif /* SRC_COMMANDS_CUBEINTAKEACTUATEOPEN_H_ */
